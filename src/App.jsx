3ce66f277885530a523bf2fe3238a57bafdc0af4
Juanchipiruli <jbruera@facultad.sanfrancisco.utn.edu.ar>
2025-05-06 21:26:54 -0300

    primer entrega TP 8/5

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Login from './components/Login'
 
 function App() {
   return (
+    <>
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
+    </>
   )
 }
 
